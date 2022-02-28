4837874cddd3ac8d9ca4d8aa3b0c9f1874d0ba04
ViktoriaB <v.chamborant@hotmail.com>
2022-02-28 14:40:03 -0600

    Strings in and output

---
+++ TaxCalc.py
@@ -0,0 +1,8 @@
+# Create a Tax Calculator Script
+
+amount = 50.54
+tax = .07
+
+total = amount + amount*tax
+
+print(total)
---
+++ strings-in-output.py
@@ -0,0 +1,28 @@
+# CONVERT FLOAT TO INT
+
+amount = int(10.6)
+print(amount)  # 10
+
+# CONVERT INT TO FLOAT
+
+amount = float(20)
+print(amount)  # 20.0
+
+# STRING
+name = 'Sarah'
+print(name)
+
+store_name = "Sarah's Store"
+print(store_name)
+
+# STRING CONCATENATION
+hello = 'Hello'
+name = 'Sarah'
+greeting = hello + " " + name
+print(greeting)
+
+# CONSOLE INPUT
+hello = 'Hello'
+name = input('What is your name? \n') # \n print in new line
+greeting = hello + " " + name
+print(greeting)
