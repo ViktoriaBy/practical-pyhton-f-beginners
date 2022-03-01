75e7f0f7df6493e6185747905db63367b003a14e
ViktoriaB <v.chamborant@hotmail.com>
2022-03-01 14:46:25 -0600

    added functions

--- roll-the-dice.py
+++ roll-the-dice.py
@@ -2,10 +2,34 @@
 
 import random
 
-roll = random.randint(1, 6)
-guess = int(input('Guess the dice roll:\n'))
+# roll = random.randint(1, 6)
+# guess = int(input('Guess the dice roll:\n'))
+#
+# if guess == roll:
+#     print("Correct! They rolled a " + str(roll))
+# else:
+#     print("Wrong! They rolled a " + str(roll))
 
-if guess == roll:
-    print("Correct! They rolled a " + str(roll))
-else:
-    print("Wrong! They rolled a " + str(roll))
+#ROLL THE DICE WITH FUNCTIONS
+def roll_dice():
+    dice_total = random.randint(1, 6) + random.randint(1, 6)
+    return dice_total
+
+def main():
+    player1 = input("Enter player1 name: ")
+    player2 = input("Enter player2 name: ")
+
+    roll1 = roll_dice()
+    roll2 = roll_dice()
+
+    print(player1, 'rolled', roll1)
+    print(player2, 'rolled', roll2)
+
+    if roll1 > roll2:
+        print(player1, 'wins!')
+    elif roll2 > roll1:
+        print(player2, 'wins!')
+    else:
+        print('You tie!')
+
+main()
--- weatherApi.py
+++ weatherApi.py
@@ -3,7 +3,7 @@ import requests
 api_key = 'b8310094bc2fd693d6caa7690335f91d'
 city = 'Dallas'
 
-url = "http://www.api.openweathermap.org/data/2.5/weather?q="+city+"&appid=b8310094bc2fd693d6caa7690335f91d"
+url = "http://www.api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b8310094bc2fd693d6caa7690335f91d"
 
 r = requests.get(url)
 json = r.json()
