a8aabaecd227b824d5397c8d5c08ddcb8668da8b
ViktoriaB <v.chamborant@hotmail.com>
2022-03-01 14:27:07 -0600

    weather api

---
+++ weatherApi.py
@@ -0,0 +1,12 @@
+import requests
+
+api_key = 'b8310094bc2fd693d6caa7690335f91d'
+city = 'Dallas'
+
+url = "http://www.api.openweathermap.org/data/2.5/weather?q="+city+"&appid=b8310094bc2fd693d6caa7690335f91d"
+
+r = requests.get(url)
+json = r.json()
+
+description = json.get('weather')[0].get('description')
+print('Todays forecast is', description)
